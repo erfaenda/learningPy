7247d735b4b7dd703aa89de9eedb5a346902d815
alex <regalko7@gmail.com>
2017-08-15 16:51:42 +0500

    наааy

--- Files/Picklee.py
+++ Files/Picklee.py
@@ -6,7 +6,7 @@ game_data = {
     'рюкзак' : ['веревка', 'молоток', 'яблоко'],
     'деньги' : 158.50
 }
-save_file = open('/home/alex/save.dat', 'wb')
+save_file = open('/home/alex/save.dat', 'wb') #wb писать фал в бинарном формате
 pickle.dump(game_data, save_file)
 save_file.close()
 print('Файл' + str(save_file) + 'успешно сохранен')
@@ -14,7 +14,7 @@ print('--------------следущая программа--------------')
 time.sleep(1)
 print('--------------идет загрузка файла-------------')
 time.sleep(2)
-load_file = open('/home/alex/save.dat', 'rb')
+load_file = open('/home/alex/save.dat', 'rb')#rb читать файл в бинарном формате
 loaded_game_date = pickle.load(load_file)
 load_file.close()
 print(loaded_game_date)
