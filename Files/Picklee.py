ea876adb1be5cf0dc21cf302a6c099c213b8541a
alex <regalko7@gmail.com>
2017-08-15 16:34:12 +0500

    наааy

---
+++ Files/Picklee.py
@@ -0,0 +1,11 @@
+import pickle
+
+game_data = {
+    'позиция-игрока' : 'C23 B45',
+    'карманы' : ['ключи', 'карманный нож', 'гладкий камень'],
+    'рюкзак' : ['веревка', 'молоток', 'яблоко'],
+    'деньги' : 158.50
+}
+save_file = open('save.dat', 'wb')
+pickle.dump(game_data, save_file)
+save_file.close()
